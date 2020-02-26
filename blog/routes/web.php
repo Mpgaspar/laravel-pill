b49d19dfaa23c684149be92ed301afb4239d7583
Marcelo Gaspar <drmpgaspar@gmail.com>
2020-02-26 01:45:40 +0100

    Create routes, controllers and views

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/users/{id}/{name}', function ($id, $name) {
+//     return 'This is user '.$name.' with an id of '.$id;
+// });
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
+
+
+
+
